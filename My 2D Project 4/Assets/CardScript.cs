fdf4609df2ad3b5b1806f4d868b87e26f586aefb
willbraun <williamhbraun1@gmail.com>
2021-10-16 17:39:40 -0400

    10/16/21 Day Session
    Did a lot today. Added sorting to the selected cards, created a 'Clear Selection' button, refactored the S10Controller2 script to pull the card GameObjects directly rather than use an abstracted card class. This shortened the code a lot, then I converted some for loops to foreach blocks for simplicity. Added a submit button, and next step is to make it functional.

--- My 2D Project 4/Assets/CardScript.cs
+++ My 2D Project 4/Assets/CardScript.cs
@@ -14,39 +14,76 @@ public class CardScript : MonoBehaviour
     // List of selected cards for positioning
     public static List<CardScript> selectedCardObjects = new List<CardScript>();
 
+    public float yposition = -95.0f;
+    public static float increment = 47.0f;
+    public float cardSpread = 0.0f;
+    public float xposition = 0.0f;
+
     void OnMouseDown()
     {
 
-        float yposition = -95.0f;
-
         if (this.transform.position.y != yposition)
         {
-            // On Select
-            // Save previous position, add card object to "selected" dictionary and list
+            // On Select, save previous position, add card object to "selected" dictionary and list
             previousPosition = this.transform.position;
             selectedCardPrevPos.Add(this,previousPosition);
             selectedCardObjects.Add(this);
         }
         else
         {
-            // On Deselect
-            // Set card back to original position, and remove from dictionary and list
+            // On deselect, set card back to original position, and remove from dictionary and list
             this.transform.position = selectedCardPrevPos[this];  
             selectedCardPrevPos.Remove(this);
             selectedCardObjects.Remove(this);
         }
         
-        // Reposition selected cards
-        float increment = 52.0f;
-        float cardSpread = increment*(selectedCardObjects.Count - 1);
-        float xposition = cardSpread*(-0.5f); // The -1 is because there are one less spaces than cards
+        // Sort and reposition selected cards
+        char[] sortArray = new char[15] {'3','4','5','6','7','8','9','1','J','Q','K','A','2','B','R'}; // Used char instead of string since name[0] returns a char
+        selectedCardObjects.Sort((x,y) => Array.IndexOf(sortArray,x.name[0]).CompareTo(Array.IndexOf(sortArray,y.name[0])));
+
+        cardSpread = increment*(selectedCardObjects.Count - 1);
+        xposition = cardSpread*(-0.5f); // The -1 is because there are one less spaces than cards
         
-        for (int i = 0; i < selectedCardObjects.Count; i++)
+        foreach (CardScript card in selectedCardObjects)
         {
-            GameObject.Find(selectedCardObjects[i].name).transform.position = new Vector2(xposition,yposition);
+            card.transform.position = new Vector2(xposition,yposition);
             xposition += increment;
         }
 
+
+        // for (int i = 0; i < selectedCardObjects.Count; i++)
+        // {
+        //     GameObject.Find(selectedCardObjects[i].name).transform.position = new Vector2(xposition,yposition);
+        //     xposition += increment;
+        // }
+
     }
+
+    public void ClearSelection()
+    {
+        foreach (KeyValuePair<CardScript,Vector2> entry in selectedCardPrevPos)
+        {
+            entry.Key.transform.position = entry.Value;
+        }
+        selectedCardPrevPos.Clear();
+        selectedCardObjects.Clear();
+    }
+
+    // public void PlayCards();
+    // {
+
+
+
+
+    // }
+
+
+
+    // void Update()
+    // {
+    //     float speed = 0.1f;
+    //     float step = speed * Time.deltaTime;
+    //     transform.position = Vector2.MoveTowards(new Vector2(transform.position.x,transform.position.y), new Vector2(xposition,yposition), step);
+    // }
 
 }
--- My 2D Project 4/Assets/S10Controller2.cs
+++ My 2D Project 4/Assets/S10Controller2.cs
@@ -12,75 +12,6 @@ public class S10Controller2 : MonoBehaviour
     [SerializeField] private GameObject MainGame;
     [SerializeField] private GameObject ExitGameButton;
 
-    [SerializeField] private GameObject SA;
-    [SerializeField] private GameObject S2;
-    [SerializeField] private GameObject S3;
-    [SerializeField] private GameObject S4;
-    [SerializeField] private GameObject S5;
-    [SerializeField] private GameObject S6;
-    [SerializeField] private GameObject S7;
-    [SerializeField] private GameObject S8;
-    [SerializeField] private GameObject S9;
-    [SerializeField] private GameObject S10;
-    [SerializeField] private GameObject SJ;
-    [SerializeField] private GameObject SQ;
-    [SerializeField] private GameObject SK;
-    [SerializeField] private GameObject HA;
-    [SerializeField] private GameObject H2;
-    [SerializeField] private GameObject H3;
-    [SerializeField] private GameObject H4;
-    [SerializeField] private GameObject H5;
-    [SerializeField] private GameObject H6;
-    [SerializeField] private GameObject H7;
-    [SerializeField] private GameObject H8;
-    [SerializeField] private GameObject H9;
-    [SerializeField] private GameObject H10;
-    [SerializeField] private GameObject HJ;
-    [SerializeField] private GameObject HQ;
-    [SerializeField] private GameObject HK;
-    [SerializeField] private GameObject CA;
-    [SerializeField] private GameObject C2;
-    [SerializeField] private GameObject C3;
-    [SerializeField] private GameObject C4;
-    [SerializeField] private GameObject C5;
-    [SerializeField] private GameObject C6;
-    [SerializeField] private GameObject C7;
-    [SerializeField] private GameObject C8;
-    [SerializeField] private GameObject C9;
-    [SerializeField] private GameObject C10;
-    [SerializeField] private GameObject CJ;
-    [SerializeField] private GameObject CQ;
-    [SerializeField] private GameObject CK;
-    [SerializeField] private GameObject DA;
-    [SerializeField] private GameObject D2;
-    [SerializeField] private GameObject D3;
-    [SerializeField] private GameObject D4;
-    [SerializeField] private GameObject D5;
-    [SerializeField] private GameObject D6;
-    [SerializeField] private GameObject D7;
-    [SerializeField] private GameObject D8;
-    [SerializeField] private GameObject D9;
-    [SerializeField] private GameObject D10;
-    [SerializeField] private GameObject DJ;
-    [SerializeField] private GameObject DQ;
-    [SerializeField] private GameObject DK;
-    [SerializeField] private GameObject RJ;
-    [SerializeField] private GameObject BJ;
-
-    [SerializeField] private GameObject selectedCards;
-    [SerializeField] private GameObject player2;
-    [SerializeField] private GameObject player3;
-    [SerializeField] private GameObject player4;
-
-
-    class card
-    {
-        public string val { get; set; }
-        public string suit { get; set; }
-        public int index { get; set; }
-        public string GameObjectName { get; set; }
-    }
-
     public void Start()
     {
         StartPage.SetActive(true);
@@ -91,63 +22,10 @@ public class S10Controller2 : MonoBehaviour
         StartPage.SetActive(false);
         
         // Define deck
-        List<card> deck = new List<card> {
-            new card {val = "2", suit = "clubs", index = 0, GameObjectName = "2ofclubs"},
-            new card {val = "3", suit = "clubs", index = 1, GameObjectName = "3ofclubs"},
-            new card {val = "4", suit = "clubs", index = 2, GameObjectName = "4ofclubs"},
-            new card {val = "5", suit = "clubs", index = 3, GameObjectName = "5ofclubs"},
-            new card {val = "6", suit = "clubs", index = 4, GameObjectName = "6ofclubs"},
-            new card {val = "7", suit = "clubs", index = 5, GameObjectName = "7ofclubs"},
-            new card {val = "8", suit = "clubs", index = 6, GameObjectName = "8ofclubs"},
-            new card {val = "9", suit = "clubs", index = 7, GameObjectName = "9ofclubs"},
-            new card {val = "10", suit = "clubs", index = 8, GameObjectName = "10ofclubs"},
-            new card {val = "J", suit = "clubs", index = 9, GameObjectName = "Jackofclubs"},
-            new card {val = "Q", suit = "clubs", index = 10, GameObjectName = "Queenofclubs"},
-            new card {val = "K", suit = "clubs", index = 11, GameObjectName = "Kingofclubs"},
-            new card {val = "A", suit = "clubs", index = 12, GameObjectName = "Aceofclubs"},
-
-            new card {val = "2", suit = "diamonds", index = 13, GameObjectName = "2ofdiamonds"},
-            new card {val = "3", suit = "diamonds", index = 14, GameObjectName = "3ofdiamonds"},
-            new card {val = "4", suit = "diamonds", index = 15, GameObjectName = "4ofdiamonds"},
-            new card {val = "5", suit = "diamonds", index = 16, GameObjectName = "5ofdiamonds"},
-            new card {val = "6", suit = "diamonds", index = 17, GameObjectName = "6ofdiamonds"},
-            new card {val = "7", suit = "diamonds", index = 18, GameObjectName = "7ofdiamonds"},
-            new card {val = "8", suit = "diamonds", index = 19, GameObjectName = "8ofdiamonds"},
-            new card {val = "9", suit = "diamonds", index = 20, GameObjectName = "9ofdiamonds"},
-            new card {val = "10", suit = "diamonds", index = 21, GameObjectName = "10ofdiamonds"},
-            new card {val = "J", suit = "diamonds", index = 22, GameObjectName = "Jackofdiamonds"},
-            new card {val = "Q", suit = "diamonds", index = 23, GameObjectName = "Queenofdiamonds"},
-            new card {val = "K", suit = "diamonds", index = 24, GameObjectName = "Kingofdiamonds"},
-            new card {val = "A", suit = "diamonds", index = 25, GameObjectName = "Aceofdiamonds"},
-
-            new card {val = "2", suit = "hearts", index = 26, GameObjectName = "2ofhearts"},
-            new card {val = "3", suit = "hearts", index = 27, GameObjectName = "3ofhearts"},
-            new card {val = "4", suit = "hearts", index = 28, GameObjectName = "4ofhearts"},
-            new card {val = "5", suit = "hearts", index = 29, GameObjectName = "5ofhearts"},
-            new card {val = "6", suit = "hearts", index = 30, GameObjectName = "6ofhearts"},
-            new card {val = "7", suit = "hearts", index = 31, GameObjectName = "7ofhearts"},
-            new card {val = "8", suit = "hearts", index = 32, GameObjectName = "8ofhearts"},
-            new card {val = "9", suit = "hearts", index = 33, GameObjectName = "9ofhearts"},
-            new card {val = "10", suit = "hearts", index = 34, GameObjectName = "10ofhearts"},
-            new card {val = "J", suit = "hearts", index = 35, GameObjectName = "Jackofhearts"},
-            new card {val = "Q", suit = "hearts", index = 36, GameObjectName = "Queenofhearts"},
-            new card {val = "K", suit = "hearts", index = 37, GameObjectName = "Kingofhearts"},
-            new card {val = "A", suit = "hearts", index = 38, GameObjectName = "Aceofhearts"},
-
-            new card {val = "2", suit = "spades", index = 39, GameObjectName = "2ofspades"},
-            new card {val = "3", suit = "spades", index = 40, GameObjectName = "3ofspades"},
-            new card {val = "4", suit = "spades", index = 41, GameObjectName = "4ofspades"},
-            new card {val = "5", suit = "spades", index = 42, GameObjectName = "5ofspades"},
-            new card {val = "6", suit = "spades", index = 43, GameObjectName = "6ofspades"},
-            new card {val = "7", suit = "spades", index = 44, GameObjectName = "7ofspades"},
-            new card {val = "8", suit = "spades", index = 45, GameObjectName = "8ofspades"},
-            new card {val = "9", suit = "spades", index = 46, GameObjectName = "9ofspades"},
-            new card {val = "10", suit = "spades", index = 47, GameObjectName = "10ofspades"},
-            new card {val = "J", suit = "spades", index = 48, GameObjectName = "Jackofspades"},
-            new card {val = "Q", suit = "spades", index = 49, GameObjectName = "Queenofspades"},
-            new card {val = "K", suit = "spades", index = 50, GameObjectName = "Kingofspades"},
-            new card {val = "A", suit = "spades", index = 51, GameObjectName = "Aceofspades"}
-        };
+        var deckArray = GameObject.FindGameObjectsWithTag("Card");
+        List<GameObject> deck = new List<GameObject>(deckArray);
+        deck.Remove(GameObject.Find("Redjoker"));
+        deck.Remove(GameObject.Find("Blackjoker"));
 
         // Shuffle deck - for 1000 turns, switch the values of two random cards
         int deckSize = deck.Count;
@@ -165,11 +43,11 @@ public class S10Controller2 : MonoBehaviour
 
         // Deal cards evenly
         int numPlayers = 4;
-        List<List<card>> hands = new List<List<card>>();
+        List<List<GameObject>> hands = new List<List<GameObject>>();
         
         for (int i = 0; i < numPlayers; i++) // Creates a list of empty lists, one for each player
         {
-            hands.Add(new List<card> {});
+            hands.Add(new List<GameObject> {});
         }
 
         int n = 0;
@@ -185,30 +63,30 @@ public class S10Controller2 : MonoBehaviour
         }
 
         // Display all player cards (players 2-4 outside viewing area)
-        string[] sortArray = new string[14] {"3","4","5","6","7","8","9","10","J","Q","K","A","2","Joker"};
+        char[] sortArray = new char[15] {'3','4','5','6','7','8','9','1','J','Q','K','A','2','B','R'};
         
-        int[] xPosArray = new int[4] {0,0,0,0};
         int[] yPosArray = new int[4] {-205,475,400,325};
+        int yPosArrayIndex = 0;
 
-        for (int h = 0; h < hands.Count; h++)
+        foreach (List<GameObject> hand in hands)
         {
-            hands[h].Sort((x,y) => Array.IndexOf(sortArray,x.val).CompareTo(Array.IndexOf(sortArray,y.val)));
+            hand.Sort((x,y) => Array.IndexOf(sortArray,x.name[0]).CompareTo(Array.IndexOf(sortArray,y.name[0])));
 
-            float increment = 52.0f;
-            float cardSpread = increment*(hands[h].Count - 1); // The -1 is because there are one less spaces than cards
-            float xposition = cardSpread*(-0.5f) + xPosArray[h];
-            float yposition = yPosArray[h];
+            float increment = 47.0f;
+            float cardSpread = increment*(hand.Count - 1); // The -1 is because there are one less spaces than cards
+            float xposition = cardSpread*(-0.5f);
+            float yposition = yPosArray[yPosArrayIndex];
 
-            for (int i = 0; i < hands[h].Count; i++)
+            foreach (GameObject card in hand)
             {
-                GameObject.Find(hands[h][i].GameObjectName).transform.position = new Vector2(xposition,yposition);
+                card.transform.position = new Vector2(xposition,yposition);
                 xposition += increment;
             }
+
+            yPosArrayIndex += 1;
         }
      
     }
-
-    
 
     public void ExitGame()
     {
